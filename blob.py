626033c213e4c8f3679b0f06cff4849bc5655cb8
spartan117 <soumyaupadhyay.6@gmail.com>
2019-08-01 09:03:13 +0200

    Update blob.py

--- blob.py
+++ blob.py
@@ -7,7 +7,7 @@ def upload(file_name, path):
     print("Ruuning blob setup")
     sys.stdout.flush()
     try:
-        block_blob_service = BlockBlobService(account_name='finnminingblob', account_key='rCzjwJWgDl10l+9SBw1C0iWnKOMDmN9PES083C+gim+ZPEbfPg6rLU4D4bk/URNw2CLxG2e8bWQzW3C4QROhWw==')
+        block_blob_service = BlockBlobService(account_name='accountName', account_key='accountKey')
         block_blob_service.create_container(container_name)
         # Set the permission so the blobs are public.
         block_blob_service.set_container_acl(container_name, public_access=PublicAccess.Container)
