8d5aa723afd9131132170cc84130d9c120ece897
Soumya <soumya@appeartv.com>
2019-09-09 13:42:56 +0200

    Increase cpu cores for google cloud

--- main.py
+++ main.py
@@ -6,63 +6,52 @@ import parse_link as link
 import parse_price as price
 import parse_sold as sold
 import parse_visning as visning
-from flask import request
 import sys
 import blob
+import datetime
 
 @app.route('/links')
 def renderLinks():
-    print("Request received for links..")
+    print("Request received for links at {}".format(datetime.datetime.now()))
     sys.stdout.flush()
-    scan = request.args.get('scan')
-    linksData = {}
-    if scan:
-        print("Scanning links..")
-        sys.stdout.flush()
-        linksBlob = blob.readBlob('links.json')
-        linksData = link.parseTitle(linksBlob)
+    linksBlob = blob.readBlob('links.json')
+    linksData = link.parseTitle(linksBlob)
     blob.writeBlob("links.json", linksData)
+    print("Request finished for links at {}".format(datetime.datetime.now()))
     return ""
 
 @app.route('/price')
 def renderPrice():
-    print("Request receieved for price.")
+    print("Request received for price at {}".format(datetime.datetime.now()))
     sys.stdout.flush()
-    scan = request.args.get('scan')
-    pris = None
-    multiple = None
-    if scan:
-        print("Scanning price..")
-        sys.stdout.flush()
-        linkBlob = blob.readBlob('links.json')
-        prisBlob = blob.readBlob('pris.json')
-        multipleBlob = blob.readBlob('multiplePris.json')
-        pris = price.parsePrice(linkBlob, prisBlob)
-        multiple = price.multiplePriceLinks(multipleBlob, prisBlob)
+    linkBlob = blob.readBlob('links.json')
+    prisBlob = blob.readBlob('pris.json')
+    multipleBlob = blob.readBlob('multiplePris.json')
+    pris = price.parsePrice(linkBlob, prisBlob)
+    multiple = price.multiplePriceLinks(multipleBlob, prisBlob)
 
     blob.writeBlob("multiplePris.json", multiple)
     blob.writeBlob("pris.json", pris)
+    print("Request finished for price at {}".format(datetime.datetime.now()))
     return ""
 
 @app.route('/visning')
 def renderVisning():
-    print("Request received for visning.")
+    print("Request received for visnings at {}".format(datetime.datetime.now()))
     sys.stdout.flush()
-    scan = request.args.get('scan')
-    visnings = None
-    if scan:
-        print("Scanning visnings..")
-        sys.stdout.flush()
-        linkBlob = blob.readBlob('links.json')
-        visningBlob = blob.readBlob('visning.json')
-        visnings = visning.parseVisning(linkBlob, visningBlob)
+    print("Scanning visnings..")
+    sys.stdout.flush()
+    linkBlob = blob.readBlob('links.json')
+    visningBlob = blob.readBlob('visning.json')
+    visnings = visning.parseVisning(linkBlob, visningBlob)
 
     blob.writeBlob("visning.json", visnings)
+    print("Request finished for visnings at {}".format(datetime.datetime.now()))
     return ""
 
 @app.route('/clean')
 def removeSoldData():
-    print("Running cleanup..")
+    print("Request received for cleanup at {}".format(datetime.datetime.now()))
     sys.stdout.flush()
     linkBlob = blob.readBlob('links.json')
     prisBlob = blob.readBlob('pris.json')
@@ -73,20 +62,33 @@ def removeSoldData():
 
     blob.writeBlob("links.json", linkData)
     blob.writeBlob("pris.json", prisData)
+    print("Request finished for cleanup at {}".format(datetime.datetime.now()))
     return ""
 
 @app.route('/sold')
-def renderSold(): 
-    print("Request receieved for sold..")
+def renderSold():
+    print("Request received for sold at {}".format(datetime.datetime.now()))
     sys.stdout.flush()
-    scan = request.args.get('scan')
-    soldData = None
-    if scan:
-        print("Scanning sold..")
-        sys.stdout.flush()
-        linkBlob = blob.readBlob('links.json')
-        soldBlob = blob.readBlob('sold.json')
-        soldData = sold.parseSold(linkBlob, soldBlob)
+    print("Scanning sold..")
+    sys.stdout.flush()
+    linkBlob = blob.readBlob('links.json')
+    soldBlob = blob.readBlob('sold.json')
+    soldData = sold.parseSold(linkBlob, soldBlob)
 
     blob.writeBlob("sold.json", soldData)
+    print("Request finished for sold at {}".format(datetime.datetime.now()))
     return ""
+
+def request():
+    print("Sending requests at {}: ".format(datetime.datetime.now()))
+    # threading.Timer(1200.0, request).start()
+    renderLinks()
+    renderPrice()
+    renderSold()
+    renderVisning()
+    removeSoldData()
+    print("Finished at: {}!".format(datetime.datetime.now()))
+
+# if __name__== "__main__":
+#     print("Running main!!")
+    # request()
